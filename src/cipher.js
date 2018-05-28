00081a92c56a2f54fc4291ab53d492e50b32b412
julissa <rosmery0239@gnail.com>
2018-05-28 18:47:39 -0500

    se modifico el html el cipher y el js  para letras mayusculas y minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,64 @@
-window.cipher = {
-  encode: (string, offset)=>{
-    debugger
-  let suma=(string.charCodeAt(0)-65+offset)%26+65;
-  return String.fromCharCode(suma);
-  
+  window.cipher = {
 
-    
-  }
-  };
-
+    encode:  (string, offset) => {
+      //pasrseIn para especificar numeros enteros
+      offset=parseInt(offset);
+       //mostrar cifra codificada
+      let codCipher='';
+      //esto se lee i=va a comensar en 0 i= va a ser mayor o igual que  i= va a avanzar de uno en uno.
+      for (let i=0; i<string.length; i++){
+        //a es el codigo ascii
+        let a=string.charCodeAt(i);
+        //if= si..a es mayor o igual y menor o igual que
+        if (a>=65 && a<=90){
+        //x = mayusculas
+        let x=(a-65+offset)%26+65;
+        codCipher = codCipher+String.fromCharCode(x);//parapasarlo de numeros a letras 
+        }
+        //if= si..a es mayor o igual y menor o igual que
+        if (a>=97 && a<=122){
+          //y =ninusculas 
+          let y=(a-97+offset)%26+97;
+           codCipher=codCipher+String.fromCharCode(y);//parapasarlo de numeros a letras 
+        }
+         //si es igual
+        if(a==32){
+          let space="";
+          codCipher+=space;
+        }
+        }
+        //regresa
+        return codCipher;
+      
+      },
+      decode:  (string, offset) => {
+        //pasrseIn para especificar numeros enteros
+        offset=parseInt(offset);
+        //mostrar cifra decodificada
+        let decodCipher='';
+        //for= para ..esto se lee i=va a comensar en 0 i= va a ser mayor o igual que  i= va a avanzar de uno en uno.
+        for (let i=0; i<string.length; i++){
+          //a es codigo asccii
+          let a=string.charCodeAt(i);
+          //if= si..a es mayor o igual y menor o igual que
+          if(a>=65 && a<=90){
+            //x=mayusculas
+          let x=(a+65-offset)%26+65;
+          decodCipher = decodCipher+String.fromCharCode(x);//parapasarlo de numeros a letras 
+          }
+           //if= si..a es mayor o igual y menor o igual que
+          if(a>=97 && a<=122){
+            //y =ninusculas
+            let y=(a+97-offset-12)%26+97;
+            decodCipher=decodCipher+String.fromCharCode(y);//parapasarlo de numeros a letras
+          }
+          //si es igual
+          if(a==32){
+            let space="";
+            decodCipher+=space;
+          }
+          }
+          //regresa
+          return decodCipher;
+        }
+      };
--- src/index.js
+++ src/index.js
@@ -1,40 +1,32 @@
-//se crea lafunction sifrad qu incluye el primer textarea.
+//se crea la function del boton sifrad
 function sifrad(){
+    // se crea una variable tex que llama a textarea por su id cifra
 let tex = document.getElementById('cifra').value ;
-//charCodeAt(tex);
+//para que tambien aparesca en la consola
 console.log(tex);
 
-
+//se crea una variable paran el imput offset y asi el usuario elija sus espacios
 let posicion=parseInt(document.getElementById('offset').value);
+//para que tambien aparesca en la consola
 console.log(posicion);
+//se obtiene el elemento con el id =respuesta del texarea=area de texto ..para grabar el resultado
+//y se le llama del html y vincula la formula de encode =codificar insertandola en tex y posicion
 document.getElementById("respuesta").innerHTML=window.cipher.encode(tex,posicion);
 }
-
+//se crea la function del boton desc
 function desc(){
-    let tex = document.getElementById('decifra').value ;
-//charCodeAt(tex);
+     // se crea una variable tex que llama a textarea por su id cifra
+let tex = document.getElementById('decifra').value ;
+//para que tambien aparesca en la consola
 console.log(tex);
-// para ver tus resultados
-document.getElementById("respuesta").innerHTML=tex;
-//esto es para vincularlo con el elector de numeros o espacios
-let posicion=document.getElementById('offset').value;
+//se crea una variable paran el imput offset y asi el usuario elija sus espacios
+let posicion=parseInt(document.getElementById('offset').value);
+//para que tambien aparesca en la consola
 console.log(posicion);
+//se obtiene el elemento con el id =respuesta del texarea=area de texto ..para grabar el resultado
+//y se le llama del html y vincula la formula de decode =codificar insertandola en tex y posicion
+document.getElementById("respuesta").innerHTML=window.cipher.decode(tex,posicion);
+
+
+
 }
-
-
-
-//document.getElementById("respuesta").innerHTML('tex');
-/*} 
-let fuera =document.getElementById("offset")
-fuera.charCodeAt("offset")
-
-let desifrar = document.getElementById("cifrado")
-//desifrar.charCodeAt("cifrado")
-
-let descifrado = document.getElementById("descifrado")
-//descifrado.charCodeAt("descifrado")*/
-
-
-
-
-
