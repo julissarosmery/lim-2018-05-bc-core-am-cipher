f6a644edb09b7bc0123bd91111e39fd58dcc2384
julissa <rosmery0239@gmail.com>
2018-05-29 18:42:41 -0500

    cambios en stylos

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
+let tex;
+let posicion;
+let offset1;
 //se crea la function del boton sifrad
-function sifrad(){
+let resultCifred = () => {
     // se crea una variable tex que llama a textarea por su id cifra
-let tex = document.getElementById('cifra').value ;
-//para que tambien aparesca en la consola
-console.log(tex);
-
-//se crea una variable paran el imput offset y asi el usuario elija sus espacios
-let posicion=parseInt(document.getElementById('offset').value);
-//para que tambien aparesca en la consola
-console.log(posicion);
-//se obtiene el elemento con el id =respuesta del texarea=area de texto ..para grabar el resultado
-//y se le llama del html y vincula la formula de encode =codificar insertandola en tex y posicion
-document.getElementById("respuesta").innerHTML=window.cipher.encode(tex,posicion);
+    let tex = document.getElementById('cifra').value;
+    //para que tambien aparesca en la consola
+    //se crea una variable paran el imput offset y asi el usuario elija sus espacios
+    let posicion = parseInt(document.getElementById('offset').value);
+    offset1= parseInt(posicion);
+    //para que tambien aparesca en la consola
+    console.log(posicion);
+    //se obtiene el elemento con el id =respuesta del texarea=area de texto ..para grabar el resultado
+    //y se le llama del html y vincula la formula de encode =codificar insertandola en tex y posicion
+    let result = cipher.encode(tex, posicion);
+    document.getElementById("respuesta").innerHTML = result;
 }
-//se crea la function del boton desc
-function desc(){
-     // se crea una variable tex que llama a textarea por su id cifra
-let tex = document.getElementById('decifra').value ;
-//para que tambien aparesca en la consola
-console.log(tex);
-//se crea una variable paran el imput offset y asi el usuario elija sus espacios
-let posicion=parseInt(document.getElementById('offset').value);
-//para que tambien aparesca en la consola
-console.log(posicion);
-//se obtiene el elemento con el id =respuesta del texarea=area de texto ..para grabar el resultado
-//y se le llama del html y vincula la formula de decode =codificar insertandola en tex y posicion
-document.getElementById("respuesta").innerHTML=window.cipher.decode(tex,posicion);
+document.getElementById("cifra").addEventListener('click', resultCifred);
 
 
+// //se crea la function del boton desc
+// function desc() {
+//     // se crea una variable tex que llama a textarea por su id cifra
+//     let tex = document.getElementById('decifra').value;
+//     //para que tambien aparesca en la consola
+//     console.log(tex);
+//     //se crea una variable paran el imput offset y asi el usuario elija sus espacios
+//     let posicion = parseInt(document.getElementById('offset').value);
+//     //para que tambien aparesca en la consola
+//     console.log(posicion);
+//     //se obtiene el elemento con el id =respuesta del texarea=area de texto ..para grabar el resultado
+//     //y se le llama del html y vincula la formula de decode =codificar insertandola en tex y posicion
+//     document.getElementById("respuesta").innerHTML = window.cipher.decode(tex, posicion);
 
-}
