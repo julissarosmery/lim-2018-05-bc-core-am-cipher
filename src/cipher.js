3c6e771bbc536b1f858e222c911843da35cf94e3
julissa <rosmery0239@gmail.com>
2018-05-30 17:27:14 -0500

    modificando para que se incluya el espacio en el texto ...resuktados optimos :)

--- src/cipher.js
+++ src/cipher.js
@@ -2,10 +2,10 @@
 
     encode:  (string, offset) => {
       //pasrseIn para especificar numeros enteros
-      offset=parseInt(offset);
+      
        //mostrar cifra codificada
       let codCipher='';
-      //esto se lee i=va a comensar en 0 i= va a ser mayor o igual que  i= va a avanzar de uno en uno.
+      //i= es un contador ..esto se lee i=va a comensar en 0 i= va a ser mayor o igual que  i= va a avanzar de uno en uno.
       for (let i=0; i<string.length; i++){
      // if(string[i]!=''){
         
@@ -14,14 +14,17 @@
         //if= si..a es mayor o igual y menor o igual que
         if (a>=65 && a<=90){
         //x = mayusculas
-        let x=(a-65+offset)%26+65;
+        let x=(a-65+parseInt(offset))%26+65;
         codCipher = codCipher+String.fromCharCode(x);//parapasarlo de numeros a letras 
         }
         //if= si..a es mayor o igual y menor o igual que
-        if (a>=97 && a<=122){
+         if (a>=97 && a<=122){
           //y =ninusculas 
-          let y=(a-97+offset)%26+97;
+        let y=(a-97+parseInt(offset))%26+97;
            codCipher=codCipher+String.fromCharCode(y);//parapasarlo de numeros a letras 
+        }
+        else {
+          codCipher+= string[i];
         }
           //else {
               //codCipher +='';
@@ -41,7 +44,7 @@
 
       decode:  (string, offset) => {
         //pasrseIn para especificar numeros enteros
-        offset=parseInt(offset);
+        //offset=parseInt(offset);
         //mostrar cifra decodificada
         let decodCipher='';
         //for= para ..esto se lee i=va a comensar en 0 i= va a ser mayor o igual que  i= va a avanzar de uno en uno.
@@ -51,20 +54,23 @@
           //if= si..a es mayor o igual y menor o igual que
           if(a>=65 && a<=90){
             //x=mayusculas
-          let x=(a+65-offset)%26+65;
+          let x=(a+65-parseInt(offset))%26+65;
           decodCipher = decodCipher+String.fromCharCode(x);//parapasarlo de numeros a letras 
           }
            //if= si..a es mayor o igual y menor o igual que
           if(a>=97 && a<=122){
             //y =ninusculas
-            let y=(a+97-offset-12)%26+97;
+            let y=(a+97-parseInt(offset)-12)%26+97;
             decodCipher=decodCipher+String.fromCharCode(y);//parapasarlo de numeros a letras
           }
-          //si es igual
-          if(a==32){
-            let space="";
-            decodCipher+=space;
+          else{
+            decodCipher+=string[i];
           }
+          //si es igual
+          //if(a==32){
+            //let space="";
+            //decodCipher+=space;
+          
           }
           //regresa
           return decodCipher;
