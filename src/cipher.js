6805c36c57200f2fa9576ab4d6a89ff7ccdb6a96
julissa <rosmery0239@gmail.com>
2018-06-01 02:00:43 -0500

    cambios del dia 1 /06/18 html js chiper listo

--- src/cipher.js
+++ src/cipher.js
@@ -1,81 +1,39 @@
-  window.cipher = {
-    //createCipherWithOffset() {
-     // return NaN
-    //},
-
-    encode:  (string, offset) => {
-      //pasrseIn para especificar numeros enteros
-      
-       //mostrar cifra codificada
-      let codCipher='';
-      //i= es un contador ..esto se lee i=va a comensar en 0 i= va a ser mayor o igual que  i= va a avanzar de uno en uno.
-      for (let i=0; i<string.length; i++){
-     // if(string[i]!=''){
-        
-        //a es el codigo ascii
-        let a=string.charCodeAt(i);
-        //if= si..a es mayor o igual y menor o igual que
-        if (a>=65 && a<=90){
-        //x = mayusculas
-        let x=(a-65+parseInt(offset))%26+65;
+ window.cipher = {
+  encode:  (string, offset) => {
+     let codCipher=''; //mostrar cifra codificada
+     for (let i=0; i<string.length; i++){ //esto se lee el  contador  i=va a comensar en 0 i= va a ser mayor o igual que  i= va a avanzar de uno en uno.
+      let a=string.charCodeAt(i);//a es el codigo ascii
+      if (a>=65 && a<=90){//if= si..a es mayor o igual y menor o igual que
+        let x=(a-65+parseInt(offset))%26+65; //x = mayusculas
         codCipher = codCipher+String.fromCharCode(x);//parapasarlo de numeros a letras 
+      }
+      else if (a>=97 && a<=122){ //if= si..a es mayor o igual y menor o igual que
+        let y=(a-97+parseInt(offset))%26+97; //y =ninusculas tambien se inporporo el parseado
+        codCipher=codCipher+String.fromCharCode(y);//parapasarlo de numeros a letras 
+      }else {
+        codCipher+= string[i]; //de lo contrario el codigo cifrado es igual al codigo cifrado de el contador 
+      }
+    }
+    return codCipher;//respuesta
+  },
+  decode:  (string, offset) => {
+    let decodCipher='';//mostrar cifra decodificada
+    for (let i=0; i<string.length; i++){//for= para ..esto se lee el contador  i=va a comensar en 0 i= va a ser mayor o igual que  i= va a avanzar de uno en uno.
+      let a=string.charCodeAt(i); //a es codigo asccii
+      if(a>=65 && a<=90){ //if= si..a es mayor o igual y menor o igual que
+        let x=(a+65-parseInt(offset))%26+65; //x=mayuscul
+        decodCipher = decodCipher+String.fromCharCode(x);//parapasarlo de numeros a letras
+      }
+      else if(a>=97 && a<=122){ //if= si..a es mayor o igual y menor o igual que
+        let y=((a+97-parseInt(offset))-12)%26+97; //y =ninusculas
+         decodCipher=decodCipher+String.fromCharCode(y);//parapasarlo de numeros a letras
         }
-        //if= si..a es mayor o igual y menor o igual que
-         if (a>=97 && a<=122){
-          //y =ninusculas 
-        let y=(a-97+parseInt(offset))%26+97;
-           codCipher=codCipher+String.fromCharCode(y);//parapasarlo de numeros a letras 
-        }
-        else {
-          codCipher+= string[i];
-        }
-          //else {
-              //codCipher +='';
-          //}
-          
-         //si es igual
-        //else(a==32){
-          //let space="";
-          //codCipher+=space;
-        
-        }
-        //regresa
-        return codCipher;
-    
-    
-      },
-
-      decode:  (string, offset) => {
-        //pasrseIn para especificar numeros enteros
-        //offset=parseInt(offset);
-        //mostrar cifra decodificada
-        let decodCipher='';
-        //for= para ..esto se lee i=va a comensar en 0 i= va a ser mayor o igual que  i= va a avanzar de uno en uno.
-        for (let i=0; i<string.length; i++){
-          //a es codigo asccii
-          let a=string.charCodeAt(i);
-          //if= si..a es mayor o igual y menor o igual que
-          if(a>=65 && a<=90){
-            //x=mayusculas
-          let x=(a+65-parseInt(offset))%26+65;
-          decodCipher = decodCipher+String.fromCharCode(x);//parapasarlo de numeros a letras 
-          }
-           //if= si..a es mayor o igual y menor o igual que
-          if(a>=97 && a<=122){
-            //y =ninusculas
-            let y=(a+97-parseInt(offset)-12)%26+97;
-            decodCipher=decodCipher+String.fromCharCode(y);//parapasarlo de numeros a letras
-          }
-          else{
-            decodCipher+=string[i];
-          }
-          //si es igual
-          //if(a==32){
-            //let space="";
-            //decodCipher+=space;
-          
-          }
-          //regresa
-          return decodCipher;
+        else {//si es igual
+          decodCipher+= string[i];
         }
       }
+      return decodCipher;//regresa
+    }
+  };
+      
+
--- src/index.js
+++ src/index.js
@@ -1,39 +1,17 @@
-//let tex;
-//let posicion;
-//let offset1;
-//se crea la function del boton sifrad
-let resultCifred = () => {
-     // se crea una variable tex que llama a textarea por su id cifra
-     let tex = document.getElementById('cifra').value;
-     //para que tambien aparesca en la consola
-     //se crea una variable paran el imput offset y asi el usuario elija sus espacios
-     let posicion = parseInt(document.getElementById('offset').value);
-     //offset1= parseInt(posicion);
-     //para que tambien aparesca en la consola
-     //console.log(posicion);
-     //se obtiene el elemento con el id =respuesta del texarea=area de texto ..para grabar el resultado
-     //y se le llama del html y vincula la formula de encode =codificar insertandola en tex y posicion
-     let result = cipher.encode(tex, posicion);
-     document.getElementById("respuesta").innerHTML = result;
+let resultCifred = () => {//se crea la function del boton resultado de cifrad
+    let tex = document.getElementById('cifra').value;// se crea una variable tex que llama a textarea por su id cifra
+    let posicion = parseInt(document.getElementById('offset').value);//se crea una variable paran el imput offset y asi el usuario elija sus espacios
+    //console.log(posicion);//para que tambien aparesca en la consola
+    let result = cipher.encode(tex, posicion);//se obtiene el elemento con el id =respuesta del texarea=area de texto ..para grabar el resultado..//y se le llama del html y vincula la formula de encode =codificar insertandola en tex y posicion
+    document.getElementById("respuesta").innerHTML = result;
 }
-
 document.getElementById("cifrado").addEventListener('click', resultCifred);
-
- //se crea la function del boton desc
- let resultDecifre = () => {
-     // se crea una variable tex que llama a textarea por su id cifra
-     let tex = document.getElementById('decifra').value;
-     //para que tambien aparesca en la consola
-     //se crea una variable paran el imput offset y asi el usuario elija sus espacio
-     let posicion = parseInt(document.getElementById('offset').value);
-     //offset1=parseInt(posicion);
-     //para que tambien aparesca en la consola
-     ////console.log(posicion);
-     //se obtiene el elemento con el id =respuesta del texarea=area de texto ..para grabar el resultado   
-     //y se le llama del html y vincula la formula de decode =codificar insertandola en tex y posicion
-     let r =cipher.decode(tex,posicion);
-     document.getElementById("respuesta").innerHTML=r;
+let resultDecifre = () => {//se crea la function del boton resultado de decifrar
+    let tex = document.getElementById('decifra').value;// se crea una variable tex que llama a textarea por su id cifra
+    let posicion = parseInt(document.getElementById('offset').value);//se crea una variable paran el imput offset y asi el usuario elija sus espacio
+    ////console.log(posicion); //para que tambien aparesca en la consola
+    let r =cipher.decode(tex,posicion);//se obtiene el elemento con el id =respuesta del texarea=area de texto ..para grabar el resultado   //y se le llama del html y vincula la formula de decode =codificar insertandola en tex y posicion 
+    document.getElementById("respuesta").innerHTML=r;
 }
-
-     document.getElementById("descifra").addEventListener("click",resultDecifre);
+document.getElementById("descifra").addEventListener("click",resultDecifre);
  
